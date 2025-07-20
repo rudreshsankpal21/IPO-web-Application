dd018f4727547fef321f3234dad379fb57276c48
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-20 19:24:57 +0530

    feat : add validation for registration

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,23 +2,8 @@ const express = require("express");
 const { registerUser, loginUser } = require("../controllers/authController");
 const router = express.Router();
 
-router.post(
-  "/register",
-  [
-    check("name", "Name is required").notEmpty(),
-    check("email", "Please include a valid email").isEmail(),
-    check("password", "Password must be at least 6 chars").isLength({ min: 6 }),
-  ],
-  registerUser
-);
+router.post("/register", registerUser);
 
-router.post(
-  "/login",
-  [
-    check("email", "Please include a valid email").isEmail(),
-    check("password", "Password is required").notEmpty(),
-  ],
-  loginUser
-);
+router.post("/login", loginUser);
 
 module.exports = router;
