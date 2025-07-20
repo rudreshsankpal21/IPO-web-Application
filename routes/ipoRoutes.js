400ed9a7acf7e0b7d663f436e438c586694c3a98
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-20 19:01:25 +0530

    feat : Add CRUD opearations

--- routes/ipoRoutes.js
+++ routes/ipoRoutes.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const { getAllIPOs } = require("../controllers/ipoController");
+const {
+  getAllIPOs,
+  addIPO,
+  updateIPO,
+  deleteIPO,
+  getIPO,
+} = require("../controllers/ipoController");
 
+// get all ipos
 router.get("/", getAllIPOs);
+
+// get a single ipo
+router.get("/:id", getIPO);
+
+// add new ipo
+router.post("/", addIPO);
+
+// update ipo
+router.put("/:id", updateIPO);
+
+// delete ipo
+router.delete("/:id", deleteIPO);
 
 module.exports = router;
