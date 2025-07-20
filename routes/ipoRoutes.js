51f71ea15f904cf33cd841e990e64df5b57818af
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-20 19:13:53 +0530

    feat : protected all IPO routes

--- routes/ipoRoutes.js
+++ routes/ipoRoutes.js
@@ -7,6 +7,10 @@ const {
   deleteIPO,
   getIPO,
 } = require("../controllers/ipoController");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+// protected all routes
+router.use(authMiddleware);
 
 // get all ipos
 router.get("/", getAllIPOs);
