a6f41f718ed7566d583b634b99e334aec40d946c
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-21 18:54:55 +0530

    feat : add Home page

---
+++ ipo-frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Welcome to the IPO Web App</h1>
+      <p>Here you can view and manage IPOs.</p>
+    </div>
+  );
+}
+
+export default Home;
