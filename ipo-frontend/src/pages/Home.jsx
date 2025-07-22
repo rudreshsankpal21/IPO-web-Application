01261cc12b9b2ae0a9173b84a470a3e2d3ceea1a
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-22 22:08:53 +0530

    feat : add Home page

--- ipo-frontend/src/pages/Home.jsx
+++ ipo-frontend/src/pages/Home.jsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react";
+import IPOCard from "../components/IPOCard";
+import { getAllIPOs, deleteIPO } from "../services/api";
+import "../styles/Home.css";
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+  const [ipos, setIPOs] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    loadIPOs();
+  }, []);
+
+  const loadIPOs = async () => {
+    const data = await getAllIPOs();
+    setIPOs(data);
+  };
+
+  const handleView = (id) => navigate(`/ipo/${id}`);
+  const handleEdit = (id) => navigate(`/edit/${id}`);
+  const handleDelete = async (id) => {
+    await deleteIPO(id);
+    loadIPOs();
+  };
+
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Welcome to the IPO Web App</h1>
-      <p>Here you can view and manage IPOs.</p>
+    <div>
+      <h1 style={{ padding: "20px" }}>All IPOs</h1>
+      <div className="ipo-container">
+        {ipos.map((ipo) => (
+          <IPOCard
+            key={ipo.id}
+            ipo={ipo}
+            onView={handleView}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+          />
+        ))}
+      </div>
     </div>
   );
 }
